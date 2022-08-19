e08df2f2e82aeb6c2925ab031ab06a48bb614ad7
Fernando Lopez <fer68769@gmail.com>
2022-08-19 13:55:24 -0500

    Se muestran blogs en index

--- resources/js/Layouts/Authenticated.vue
+++ resources/js/Layouts/Authenticated.vue
@@ -31,6 +31,11 @@ const showingNavigationDropdown = ref(false);
                                     Dashboard
                                 </BreezeNavLink>
                             </div>
+                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                                <BreezeNavLink :href="route('blogs.index')" :active="route().current('blogs.index')">
+                                    Blogs
+                                </BreezeNavLink>
+                            </div>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
---
+++ resources/js/Pages/Blog/Index.vue
@@ -0,0 +1,71 @@
+<script>
+    import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
+    import { Head } from '@inertiajs/inertia-vue3';
+    import  Button  from '@/Components/Button.vue';
+
+    export default {
+        components:{
+            BreezeAuthenticatedLayout,
+            Head,
+            Button
+        },
+        props:{
+            blogs: Object
+        }
+    }
+
+</script>
+<template>
+    <Head title="Blogs"></Head>
+
+    <BreezeAuthenticatedLayout>
+       <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Blogs
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="overflow-hidden">
+                    <div class="p-6">
+
+                        <section class="grid grid-cols-2 gap-4 mb-2">
+                            <div>
+                                <p>A list of all blogs in your account.</p>
+                            </div>
+                            <div>
+                                <Button class="float-right">Add blog</Button>
+                            </div>
+                        </section>
+                        <section class="border-solid border-2 border-gray-100 rounded-lg shadow-lg">
+                            <table class="auto w-full">
+                                <thead class="text-left border-solid border-b-2 border-gray-100">
+                                    <tr>
+                                        <th class="p-3 bg-white rounded-tl-lg">Title</th>
+                                        <th class="p-3 bg-white">Slug</th>
+                                        <th class="p-3 bg-white rounded-tr-lg">Content</th>
+                                        <th class="p-3 bg-white rounded-tr-lg"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr v-for="blog in blogs" :key="blog.id" class="border-solid border-b-2 border-gray-100">
+                                        <td class="p-3 bg-white">{{ blog.title }}</td>
+                                        <td class="p-3 bg-white">{{ blog.slug }}</td>
+                                        <td class="p-3 bg-white">{{ blog.content }}</td>
+                                        <td class="p-3 bg-white">
+                                            <a href="#">Edit</a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </section>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </BreezeAuthenticatedLayout>
+</template>
+
+
